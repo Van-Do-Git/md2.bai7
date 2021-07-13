41371c4cd4aef2d3d17dbe6ddfd49d713ff8d220
unknown <mittervan@gmail.com>
2021-07-13 19:16:55 +0700

    Exercise of Interface Shape

---
+++ src/ExerciseoInterfaceShape/Resizeable.java
@@ -0,0 +1,5 @@
+package ExerciseoInterfaceShape;
+
+public interface Resizeable{
+    void resize(double percent);
+}
---
+++ src/ExerciseoInterfaceShape/Square.java
@@ -0,0 +1,37 @@
+package ExerciseoInterfaceShape;
+
+public class Square extends Rectangle {
+    public Square() {
+        super(1.0, 1.0);
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String _color, boolean _filled) {
+        super(side, side, _color, _filled);
+    }
+
+    public void setSide(double side) {
+        setWidth(side);
+        setLength(side);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    @Override
+    public String toString() {
+        return "A square with side = " + getSide() + ", " +
+                "which is a subclass of " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Square square = new Square();
+        System.out.println(square);
+        square.resize(2);
+        System.out.println(square);
+    }
+}
